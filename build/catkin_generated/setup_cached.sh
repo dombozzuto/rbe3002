4662b39c6fed9516764e26e50ddf2605e48d2004
dombozzuto <dlbozzuto@wpi.edu>
2015-11-19 17:22:53 -0500

    properly rearranged files

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dombozzuto/rbe3002/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/dombozzuto/rbe3002/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/dombozzuto/rbe3002/devel/lib:/home/dombozzuto/rbe3002/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/home/dombozzuto/rbe3002/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/dombozzuto/rbe3002/devel/lib/pkgconfig:/home/dombozzuto/rbe3002/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/dombozzuto/rbe3002/build"
+export PYTHONPATH="/home/dombozzuto/rbe3002/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dombozzuto/rbe3002/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/dombozzuto/rbe3002/src:$ROS_PACKAGE_PATH"
