f212c437c210cc73dce3fc35e7b5a246c86cb5a9
dombozzuto <dlbozzuto@wpi.edu>
2015-11-21 16:31:47 -0500

    astar changes

--- build/lab4/CTestTestfile.cmake
+++ build/lab4/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/dombozzuto/rbe3002/src
-# Build directory: /home/dombozzuto/rbe3002/build
+# Source directory: /home/dombozzuto/rbe3002/src/lab4
+# Build directory: /home/dombozzuto/rbe3002/build/lab4
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(lab4)
