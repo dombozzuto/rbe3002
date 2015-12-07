2d9ace0726ca666e0a4da226a48149431ad3e0b8
pbawesome <pbawesome@gmail.com>
2015-12-07 12:53:14 -0500

    not working! progress on splitting up frontiers

--- src/lab4/Untitled Folder/finalProject.py
+++ src/lab4/Untitled Folder/finalProject.py
@@ -3,7 +3,115 @@ import rospy, roslib, tf
 import tf.transformations
 import nav_msgs.msg, geometry_msgs.msg, std_msgs.msg, kobuki_msgs.msg, actionlib_msgs.msg
 import robotStartup
+import Node
+import GridMap
 
+
+def d(node,goal):
+	dx = abs(node.xPos - goal.xPos)
+	dy = abs(node.yPos - goal.yPos)
+	return math.sqrt(abs(node.xPos - goal.xPos)**2 + abs(node.yPos - goal.yPos)**2)
+
+def findFrontiers():
+	global allnodes
+	unknownFrontierNoDist = []
+	unknownFrontier = []
+	allnodes = []
+
+	[allnodes, width, height, resolution, originx, originy]=  robotStartup.getNodeMap()
+	
+	# for node in allnodes:
+	# 	if (not (node.obs == -1)) and (node.obs <50):
+	# 		print node.printNode()
+
+	for node in allnodes:                   # For each node on the map...
+		if node.obs == -1:                  # if the node is unkown space
+			# print node.obs
+			# obtain the neighbors of that node
+			nodeNeighbors = node.getStraightNeighbors(allnodes)
+			frontierFlag = 0
+			for neigh in nodeNeighbors:
+				if (neigh.obs > -1) and (neigh.obs < 50):
+					frontierFlag = 1
+			if frontierFlag == 1:
+				unknownFrontierNoDist.append(node)
+
+	[x,y,w]=robotStartup.getPose()
+	xyscale = 1.0/(resolution*1.0)
+	currentNode=Node.Node(x,y,0,width)
+
+	robotStartup.publishGridCellNodes(unknownFrontierNoDist,4)
+	for node in unknownFrontierNoDist:
+		# calculate the distance between the current node and the current
+		# unknown frontier node
+		ppx=float((node.xPos)/xyscale)+1/(2*xyscale) + originy
+		ppy=float((node.yPos)/xyscale)+1/(2*xyscale) + originy
+		tmpnode = Node.Node(ppx,ppy,0,width)
+		dist2Node=d(currentNode,tmpnode)
+		unknownFrontier.append((dist2Node, node))  # append to list
+
+	unknownFrontier.sort(key=lambda tup: tup[0])
+	unknownFrontierNoDist = [(i[1]) for i in unknownFrontier]
+	
+	print "len allnodes", len(allnodes)
+	print "len unknownFrontier", len(unknownFrontier)
+	print "len unknownFrontierNoDist", len(unknownFrontierNoDist)
+
+	#publish GridCells to see on rviz
+	for node in unknownFrontier:
+		print(node[0],node[1].printNode())
+	for node in unknownFrontierNoDist:
+		node.printNode()
+
+	frontierGroup=[]
+	# for each node in the frontier
+	for node in unknownFrontierNoDist:
+		#find all neighbors
+		nodeNeighbors = node.getStraightNeighbors(allnodes)
+		frontierNeigh=[]
+		#for each node in the neighbors of frontier node
+		for n in nodeNeighbors:
+			#if neighbor is in frontier
+			if n in unknownFrontierNoDist:
+				#add to frontier neighbors
+				frontierNeigh.append(n)
+		frontierGroup.append(frontierNeigh)
+
+	print "\n",len(frontierGroup),len(frontierGroup[0])
+
+	for x in frontierGroup:
+		rospy.sleep(1)
+		robotStartup.publishGridCellNodes(x,3)
+
+	for m in frontierGroup:
+		print "\n",
+		for n in m:
+			n.printNode()
+
+
+
+
+	# for node in unknownFrontier:
+
+	
+	
+	# g=robotStartup.getGridMap()
+	# g.GridMap.astarSearch(g,0,0,4,4)
+def calcCentroid(frontierNodes):
+	xc=0
+	yc=0
+	cnt=0
+	for cell in unknownFrontierNoDist:
+		xc=xc+cell.xPos
+		yc=yc+cell.yPos
+		cnt=cnt+1
+	xc=xc/cnt
+	yc=yc/cnt
+	centroid=[]
+	centroid.append(Node.Node(xc,yc,1000,width))
+	robotStartup.publishGridCellNodes(centroid,3)
+
+	return Node.Node(xc,yc,1000,width)
 if __name__ == '__main__':
 	
 	rospy.init_node('finalProject', anonymous=True)
@@ -11,5 +119,10 @@ if __name__ == '__main__':
 		print "Starting"
 		rospy.sleep(1)
 		robotStartup.setupRobot()
+
+		findFrontiers()
+
+
+
 	except rospy.ROSInterruptException:
 		pass
--- src/lab4/Untitled Folder/robotStartup.py
+++ src/lab4/Untitled Folder/robotStartup.py
@@ -2,10 +2,81 @@ import math, numpy, time
 import rospy, roslib, tf
 import tf.transformations
 import nav_msgs.msg, geometry_msgs.msg, std_msgs.msg, kobuki_msgs.msg, actionlib_msgs.msg
+import Node
+
+import RobotMap
+import GridMap
 #from nav_msgs.msg import GridCells, Path
 #from geometry_msgs.msg import 
 #from nav_msgs.msg import GridCells
 #contains all the files needed to startup the robot
+#publishes a list of Point messages as GridCells
+
+scale = 1.0
+
+#publishes a list of Point messages as GridCells
+def publishGridCellList(lst,typ):
+    global resolution
+    global scale
+    gridCells = nav_msgs.msg.GridCells()
+    gridCells.header.frame_id = "/map"
+    gridCells.header.stamp = rospy.Time.now()
+    gridCells.cell_width = resolution*scale
+    gridCells.cell_height = resolution*scale
+    xyscale = 1.0/(resolution*scale)
+    
+    pntList=[]
+    for pnt in lst:
+        p = geometry_msgs.msg.Point()
+        # p.x= float(pnt.x/xyscale)+1/(2*xyscale)
+        # p.y= float(pnt.y/xyscale)+1/(2*xyscale)
+        p.x = float((pnt.x)/xyscale)+1/(2*xyscale) + originx
+        p.y = float((pnt.y)/xyscale)+1/(2*xyscale) + originy
+        p.z=0
+        pntList.append(p)
+
+    gridCells.cells=pntList
+    if(typ==0):
+        openPub.publish(gridCells)
+    if(typ==1):
+        closedPub.publish(gridCells)
+    if(typ==2):
+        pathVizPub.publish(gridCells)
+    if(typ==3):
+        astarVizPub.publish(gridCells)
+
+def publishGridCellNodes(lst,typ):
+    global resolution
+    global scale
+    gridCells = nav_msgs.msg.GridCells()
+    gridCells.header.frame_id = "/map"
+    gridCells.header.stamp = rospy.Time.now()
+    gridCells.cell_width = resolution*scale
+    gridCells.cell_height = resolution*scale
+    xyscale = 1.0/(resolution*scale)
+ 
+    pntList=[]
+    for pnt in lst:
+        p = geometry_msgs.msg.Point()
+        # p.x= float(pnt.x/xyscale)+1/(2*xyscale)
+        # p.y= float(pnt.y/xyscale)+1/(2*xyscale)
+        p.x = float((pnt.xPos)/xyscale)+1/(2*xyscale) + originx
+        p.y = float((pnt.yPos)/xyscale)+1/(2*xyscale) + originy
+        p.z=0
+        pntList.append(p)
+
+
+    gridCells.cells=pntList
+    if(typ==0):
+        openPub.publish(gridCells)
+    if(typ==1):
+        closedPub.publish(gridCells)
+    if(typ==2):
+        pathVizPub.publish(gridCells)
+    if(typ==3):
+        astarVizPub.publish(gridCells)
+    if(typ==4):
+    	frontierPub.publish(gridCells)
 
 def setupRobot():
 	print "Setting up Misc"
@@ -16,9 +87,22 @@ def setupRobot():
 	setupSubscribers()
 	print "Sleeping for 2 seconds"
 	rospy.sleep(2)
-	print "Trying to rotate"
-	initialRotate(45)
-    
+	# print "Trying to rotate"
+	# initialRotate(45)
+
+	startTime = time.time()
+	filledMap = RobotMap.map1Dto2D(width, height, mapData)
+	shrinkedMap = RobotMap.shrinkMap(width, height,filledMap)
+	doneTime = time.time()	
+
+	global gridMap
+	gridMap = GridMap.GridMap(width, height, shrinkedMap)
+	# gridMap.aStarSearch(0,0,4,4)
+
+
+
+def getGridMap():
+	return gridMap
     
 
 def initialRotate(degsToRotate):
@@ -44,6 +128,7 @@ def setupPublishers():
 	global closedPub
 	global pathVizPub
 	global astarVizPub
+	global frontierPub
 	global navStackPub
 	global pathPub
 
@@ -52,6 +137,7 @@ def setupPublishers():
 	closedPub = rospy.Publisher('/cell_path/closed', nav_msgs.msg.GridCells, queue_size=10)
 	pathVizPub = rospy.Publisher('/cell_path/path', nav_msgs.msg.GridCells, queue_size=10)
 	astarVizPub = rospy.Publisher('/cell_path/astar', nav_msgs.msg.GridCells, queue_size=10)
+	frontierPub = rospy.Publisher('/cell_path/frontier', nav_msgs.msg.GridCells, queue_size=10)
 
 	#publishes path to RVz for robot path
 	pathPub = rospy.Publisher('/path_path', nav_msgs.msg.Path,queue_size = 5)
@@ -92,6 +178,8 @@ def readGlobalCostMap(data):
     global resolution
     global originx
     global originy
+    global map2D
+
     grid = data
     mapData = data.data
     width = data.info.width
@@ -99,6 +187,20 @@ def readGlobalCostMap(data):
     resolution = data.info.resolution
     originx = data.info.origin.position.x
     originy = data.info.origin.position.y
+    map2D = RobotMap.map1Dto2D(width, height, mapData)
+
+#returns 2D map
+def get2DMap():
+	return [map2D, width, height, resolution, originx, originy]
+
+def getNodeMap():
+	nodeList = []
+	for y in range(height):
+		for x in range(width):
+			# map2D = RobotMap.map1Dto2D(width, height, mapData)
+			nodeList.append(Node.Node(x,y,map2D[y][x],width))
+
+	return [nodeList, width, height, resolution, originx, originy]
 
 #odom callback
 def odomCallback(data):
@@ -117,5 +219,8 @@ def odomCallback(data):
     yPos = y
     theta = math.degrees(yaw)
 
+def getPose():
+	return [xPos, yPos, theta]
+
 def readBaseStatus(data):
-	print "nothing to see here"
+	return 0
